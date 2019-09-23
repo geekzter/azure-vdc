31a32ec10dca3658941199459c43c9e90340bac3
Eric van Wijk <eric@van-wijk.com>
2019-09-23 17:41:32 +0200

    Added ASP.NET-SQL DB pp, moved iIS-ASP.NET app

---
+++ Apps/PaaS-ASP.NET-SQLDB/DotNetAppSqlDb/Migrations/201812070907567_AddProperty.cs
@@ -0,0 +1,16 @@
+namespace DotNetAppSqlDb.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddProperty : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Apps/PaaS-ASP.NET-SQLDB/DotNetAppSqlDb/Models/MyDatabaseContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using Microsoft.Azure.Services.AppAuthentication;
+using System.Web.Configuration;
+
+namespace DotNetAppSqlDb.Models
+{
+    public class MyDatabaseContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public MyDatabaseContext() : base("name=MyDbConnection")
+        {
+        }
+
+        // Used for AAD auth, using the Managed Identity of the web app
+        public MyDatabaseContext(SqlConnection conn) : base(conn, true)
+        {
+            conn.ConnectionString = WebConfigurationManager.ConnectionStrings["MyDbConnection"].ConnectionString;
+            // DataSource != LocalDB means app is running in Azure with the SQLDB connection string you configured
+            if (conn.DataSource != "(localdb)\\MSSQLLocalDB")
+                conn.AccessToken = (new AzureServiceTokenProvider()).GetAccessTokenAsync("https://database.windows.net/").Result;
+
+            Database.SetInitializer<MyDatabaseContext>(null);
+        }
+
+        public System.Data.Entity.DbSet<DotNetAppSqlDb.Models.Todo> Todoes { get; set; }
+    }
+}
