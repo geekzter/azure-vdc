0a5027af582a09f8eb3a8b56282acde19ca5dbe0
Eric van Wijk <eric@van-wijk.com>
2019-11-07 13:43:02 +0100

    Updated Terraform version checker to use tfenv

--- Scripts/get_tf_version.ps1
+++ Scripts/get_tf_version.ps1
@@ -1,7 +1,8 @@
 #!/usr/bin/env pwsh
 
 param ( 
-    [parameter(Mandatory=$false)][ValidateSet('Installed','Latest')][string]$Version="Installed"
+    [parameter(Mandatory=$false)][ValidateSet('Installed','Latest','Preferred')][string]$Version="Installed",
+    [parameter(Mandatory=$false)][string]$tfdirectory=$(Join-Path (Get-Item (Split-Path -parent -Path $MyInvocation.MyCommand.Path)).Parent.FullName "Terraform")
 ) 
 
 $pipeline = ![string]::IsNullOrEmpty($env:AGENT_VERSION)
@@ -22,6 +23,22 @@ switch ($Version) {
         Write-Output $terraformLatestVersion      
         if ($pipeline) {
             Write-Host “##vso[task.setvariable variable=LatestVersion;isSecret=false;isOutput=true;]$terraformLatestVersion”
+        }
+    }
+    'Preferred'    {
+        $tfenvFile = Join-Path $tfdirectory ".terraform-version"
+        if (Test-Path $tfenvFile) {
+            $terraformPreferredVersion = Get-Content $tfenvFile -TotalCount 1
+        }
+        if ([string]::IsNullOrEmpty($terraformPreferredVersion) -or ($terraformPreferredVersion -ieq "latest"))
+        {
+            # Preferred version not specified, take the latest 
+            $terraformPreferredVersion = Invoke-WebRequest -Uri https://checkpoint-api.hashicorp.com/v1/check/terraform -UseBasicParsing | Select-Object -ExpandProperty Content | ConvertFrom-Json | Select-Object -ExpandProperty "current_version"
+        }
+        Write-Output $terraformPreferredVersion      
+
+        if ($pipeline) {
+            Write-Host “##vso[task.setvariable variable=PreferredVersion;isSecret=false;isOutput=true;]$terraformPreferredVersion"
         }
     }
 }
