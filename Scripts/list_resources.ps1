3aa15501202f7e21dd075ce2923f3cf12ef58ee3
Eric van Wijk <ericvan@microsoft.com>
2019-11-10 18:38:50 +0000

    Added lease info

--- Scripts/list_resources.ps1
+++ Scripts/list_resources.ps1
@@ -3,7 +3,8 @@
 param (    
     [parameter(Mandatory=$false,HelpMessage="The environment tag to filter use")][string] $Environment,
     [parameter(Mandatory=$false,HelpMessage="The workspace tag to filter use")][string] $Workspace,
-    [parameter(Mandatory=$false)][switch]$CountsOnly=$false,
+    [parameter(Mandatory=$false)][switch]$Resources=$false,
+    [parameter(Mandatory=$false)][switch]$Summary=$false,
     [parameter(Mandatory=$false)][string]$tfdirectory=$(Join-Path (Get-Item (Split-Path -parent -Path $MyInvocation.MyCommand.Path)).Parent.FullName "Terraform"),
     [parameter(Mandatory=$false)][string]$subscription=$env:ARM_SUBSCRIPTION_ID,
     [parameter(Mandatory=$false)][string]$tenantid=$env:ARM_TENANT_ID,
@@ -24,11 +25,37 @@ if (!(Get-AzTenant -TenantId $tenantid -ErrorAction SilentlyContinue)) {
 }
 Set-AzContext -Subscription $subscription
 
-if ($CountsOnly) {
-    $resourceQuery = "Resources | where tags['application']=='Automated VDC' | summarize resourceCount=count() by tostring(tags['environment']), tostring(tags['workspace']) | order by tags_workspace asc"
-    Write-Host "`nQuery: $resourceQuery"
-    Search-AzGraph -Query $resourceQuery | Format-Table
-} else {
+# Provide at least one argument
+if (!($Resources -or $Summary)) {
+    Write-Host "Please indicate what to do by using a command-line switch"
+    Get-Help $MyInvocation.MyCommand.Definition
+    exit
+}
+
+if ($Summary) {
+    $resourceQuery = "Resources | where tags['application']=='Automated VDC' | summarize ResourceCount=count() by Environment=tostring(tags['environment']), Workspace=tostring(tags['workspace']) | order by Workspace asc"
+    Write-Host "`nQuery: `"$resourceQuery`""
+    $graphResult = Search-AzGraph -Query $resourceQuery
+
+    # Access Terraform (Azure) backend to get leases for each workspace
+    $tfConfig = $(Get-Content $tfdirectory/.terraform/terraform.tfstate | ConvertFrom-Json)
+    $backendStorageAccountName = $tfConfig.backend.config.storage_account_name
+    $backendStorageContainerName = $tfConfig.backend.config.container_name
+    $backendStorageKey = $env:ARM_ACCESS_KEY
+    $backendstorageContext = New-AzStorageContext -StorageAccountName $backendStorageAccountName -StorageAccountKey $backendStorageKey
+    $tfStateBlobs = Get-AzStorageBlob -Context $backendstorageContext -Container $backendStorageContainerName 
+    $leaseTable = @{}
+    $tfStateBlobs | ForEach-Object {
+        $leaseTable.Add($($_.Name -Replace "terraform.tfstateenv:","" -Replace "terraform.tfstate","default"),$_.ICloudBlob.Properties.LeaseStatus)
+    }
+ 
+    # Join tables
+    $graphResult | ForEach-Object {
+        Add-Member -InputObject $_ -NotePropertyName "Lease" -NotePropertyValue $leaseTable[$_.Workspace]
+    }
+    $graphResult | Format-Table
+} 
+if ($Resources) {
     $resourceQuery = "Resources | where tags['application']=='Automated VDC'"
     if ($Workspace) {
         $resourceQuery += " and tags['workspace']=='$Workspace'"         
@@ -36,10 +63,10 @@ if ($CountsOnly) {
     if ($Environment) {
         $resourceQuery += " and tags['environment']=='$Environment'"         
     }
-    $resourceQuery += " | project name, resourceGroup | order by name, resourceGroup asc"
+    $resourceQuery += " | project Name=name,ResourceGroup=resourceGroup | order by ResourceGroup asc, Name asc"
 
-    Write-Host "`nQuery: $resourceQuery"
+    Write-Host "`nQuery: `"$resourceQuery`""
     $result = Search-AzGraph -Query $resourceQuery -Subscription $subscription
-    $result | Format-Table -Property name, resourceGroup 
+    $result | Format-Table -Property Name, ResourceGroup 
     Write-Host "$($result.Count) item(s) found"
 }
