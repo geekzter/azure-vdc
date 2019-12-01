23bd6c9fe2ee63ab3fd66ce22d428ee94540c8b9
geekzter <eric@van-wijk.com>
2019-12-01 19:54:45 +0100

    Dashboard upgrade (#47)
    * Added build link to dashboard
    
    * Added cost to dashboard
    
    * Budget by environment instead of workspace
    
    * Wrong quotes used in replacement

--- Scripts/templatize_dashboard.ps1
+++ Scripts/templatize_dashboard.ps1
@@ -75,11 +75,14 @@ if ($InputFile) {
 $template = $template -Replace "/subscriptions/........-....-....-................./", "`$`{subscription`}/"
 $template = $template -Replace "${prefix}-", "`$`{prefix`}-"
 $template = $template -Replace "-${environment}-", "-`$`{environment`}-"
+$template = $template -Replace "\`"${environment}\`"", "`"`$`{environment`}`""
 $template = $template -Replace "-${suffix}", "-`$`{suffix`}"
 $template = $template -Replace "\`'${suffix}\`'", "'`$`{suffix`}'"
 $template = $template -Replace "http[s?]://[\w\.]*iisapp[\w\.]*/", "`$`{iaas_app_url`}"
 $template = $template -Replace "http[s?]://[\w\.]*webapp[\w\.]*/", "`$`{paas_app_url`}"
-$template = $template -Replace "https://dev.azure.com[^`']*`'", "`$`{release_web_url`}`'"
+#$template = $template -Replace "https://dev.azure.com[^`']*`'", "`$`{release_web_url`}`'"
+$template = $template -Replace "https://dev.azure.com[^`']*_build[^`']*`'", "`$`{build_web_url`}`'"
+$template = $template -Replace "https://dev.azure.com[^`']*_release[^`']*`'", "`$`{release_web_url`}`'"
 $template = $template -Replace "https://online.visualstudio.com[^`']*`'", "`$`{vso_url`}`'"
 
 # Check for remnants of tokens that should've been caught
