8be2476f004ca58207b3fd330b5a7e28870c600d
Eric van Wijk <ericvan@microsoft.com>
2020-04-11 08:31:58 +0000

    Test complex characters

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -10,7 +10,8 @@ resource "random_string" "password" {
   special                      = true
 # override_special             = "!@#$%&*()-_=+[]{}<>:?" # default
 # Avoid characters that may cause shell scripts to break
-  override_special             = "." 
+  override_special             = "!@#%*()-_=+[]{}:?" 
+# override_special             = "." 
 }
 
 # Random resource suffix, this will prevent name collisions when creating resources in parallel
@@ -36,7 +37,8 @@ locals {
   # Making sure all character classes are represented, as random does not guarantee that  
   workspace_location           = var.workspace_location != "" ? var.workspace_location : var.location
   automation_location          = var.automation_location != "" ? var.automation_location : local.workspace_location
-  password                     = ".Az9${random_string.password.result}"
+# password                     = ".Az9${random_string.password.result}"
+  password                     = ".Az9${random_string.password.override_special}"
   suffix                       = var.resource_suffix != "" ? lower(var.resource_suffix) : random_string.suffix.result
   environment                  = var.resource_environment != "" ? lower(var.resource_environment) : substr(lower(replace(terraform.workspace,"/a|e|i|o|u|y/","")),0,4)
   vdc_resource_group           = "${lower(var.resource_prefix)}-${lower(local.environment)}-${lower(local.suffix)}"
