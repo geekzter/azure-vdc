f03e050d19f603e2c382a68957e98274be71b119
Eric van Wijk <eric@van-wijk.com>
2020-05-23 17:22:16 +0200

    Prevent DNS name collisions

--- terraform/waf.tf
+++ terraform/waf.tf
@@ -24,7 +24,7 @@ resource "azurerm_public_ip" "waf_pip" {
 }
 
 resource "azurerm_dns_cname_record" "waf_iaas_app_cname" {
-  name                         = "${lower(var.resource_prefix)}${lower(var.resource_environment)}iisapp"
+  name                         = "${lower(var.resource_prefix)}${lower(local.environment)}iisapp"
   zone_name                    = data.azurerm_dns_zone.vanity_domain.0.name
   resource_group_name          = data.azurerm_dns_zone.vanity_domain.0.resource_group_name
   ttl                          = 300
@@ -36,7 +36,7 @@ resource "azurerm_dns_cname_record" "waf_iaas_app_cname" {
 } 
 
 resource "azurerm_dns_cname_record" "waf_paas_app_cname" {
-  name                         = "${lower(var.resource_prefix)}${lower(var.resource_environment)}webapp"
+  name                         = "${lower(var.resource_prefix)}${lower(local.environment)}webapp"
   zone_name                    = data.azurerm_dns_zone.vanity_domain.0.name
   resource_group_name          = data.azurerm_dns_zone.vanity_domain.0.resource_group_name
   ttl                          = 300
