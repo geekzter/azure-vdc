98c436a4ac51e5d2e0b4ee806540d7d605ea8bc2
Eric van Wijk <eric@van-wijk.com>
2020-05-24 13:53:20 +0200

    Wait for agents to come online using REST api

--- scripts/wait_for_agents.ps1
+++ scripts/wait_for_agents.ps1
@@ -1,33 +1,80 @@
 #!/usr/bin/env pwsh
 <# 
 .SYNOPSIS 
-    Wait for Pipeline Environment agents to come up
-    There is no CLI / REST operation on how to detect this, so simply wait for a VM to have started long enough ago
+    Wait for Pipeline Environment agents to come online
 #> 
 param ( 
-    [parameter(Mandatory=$true)][string]$ResourceGroup,
+    [Parameter(Mandatory=$true,
+    ParameterSetName="ResourceGroup")]
+    [String]
+    $ResourceGroup,
+
+    [Parameter(Mandatory=$true,
+    ParameterSetName="Environment")]
+    [String]
+    $Environment,
+    [Parameter(Mandatory=$true,
+    ParameterSetName="Environment")]
+    [String]
+    $OrganizationUrl,
+    [Parameter(Mandatory=$true,
+    ParameterSetName="Environment")]
+    [String]
+    $Project,
+    [Parameter(Mandatory=$false,
+    ParameterSetName="Environment")]
+    [String]
+    $Tag="web",
+
+    #[parameter(Mandatory=$true)][string]$ResourceGroup,
     [parameter(Mandatory=$false)][int]$TimeoutSeconds=300
 ) 
 
-$vmIDs = $(az vm list -g $ResourceGroup --query "[].id" -o tsv)
+if ($Environment) {
+    az extension add -y -n azure-devops 2>$null
 
-# Wait for stable state, VM's may be restarting now
-az vm wait --updated --ids $vmIDs -o none
+    $apiVersion="6.0-preview"
 
-Write-Information "Retrieving last VM status change timestamp..."
-$startTimeString = (az vm get-instance-view --ids $vmIDs --query "max([].instanceView.statuses[].time)" -o tsv)
+    # Discover appropriate arguments using this information:
+    # az devops invoke --org $OrganizationUrl --query "[?area=='environments']"  
 
-if ($startTimeString) {
-    Write-Host "VM's last started $startTimeString"
-    $startTime = [datetime]::Parse($startTimeString)
-    $waitUntil = $startTime.AddSeconds($TimeoutSeconds)
-} else {
+    $environmentId = $(az devops invoke --org $OrganizationUrl --area environments --api-version $apiVersion --route-parameters project=$Project resource=environments --resource environments --query "value[?name=='$Environment'].id" -o tsv)
+    $agents = $(az devops invoke --org $OrganizationUrl --area environments --api-version $apiVersion --route-parameters project=$Project environmentId=$environmentId --resource vmresource --query "value[?contains(tags,'$Tag') && agent.enabled].agent.name" -o tsv)
+    Write-Information "Checking status of environment agents $agents..."
+    $offlineAgents = $(az devops invoke --org $OrganizationUrl --area environments --api-version $apiVersion --route-parameters project=$Project environmentId=$environmentId --resource vmresource --query "value[?contains(tags,'$Tag') && agent.enabled && agent.status=='offline'].agent.name" -o tsv)
     $waitUntil = (Get-Date).AddSeconds($TimeoutSeconds)
+    if (!$offlineAgents) {
+        Write-Host "Waiting for $agents to come online ..." -NoNewline
+    }
+    while ($offlineAgents -and ((Get-Date) -lt $waitUntil)) {
+        Write-Host "." -NoNewLine
+        $offlineAgents = $(az devops invoke --org $OrganizationUrl --area environments --api-version $apiVersion --route-parameters project=$Project environmentId=$environmentId --resource vmresource --query "value[?contains(tags,'$Tag') && agent.enabled && agent.status=='offline'].agent.name" -o tsv)
+        Start-Sleep -Seconds 5
+    }
+    Write-Host "✓" # Force NewLine
 }
 
-$sleepTime = ($waitUntil - (Get-Date))
+if ($ResourceGroup) {
+    $vmIDs = $(az vm list -g $ResourceGroup --query "[].id" -o tsv)
 
-if ($sleepTime -gt 0) {
-    Write-Host "Sleeping $([math]::Ceiling($sleepTime.TotalSeconds)) seconds..."
-    Start-Sleep -Milliseconds $sleepTime.TotalMilliseconds
+    # Wait for stable state, VM's may be restarting now
+    az vm wait --updated --ids $vmIDs -o none
+    
+    Write-Information "Retrieving last VM status change timestamp..."
+    $startTimeString = (az vm get-instance-view --ids $vmIDs --query "max([].instanceView.statuses[].time)" -o tsv)
+    
+    if ($startTimeString) {
+        Write-Host "Last VM started $startTimeString"
+        $startTime = [datetime]::Parse($startTimeString)
+        $waitUntil = $startTime.AddSeconds($TimeoutSeconds)
+    } else {
+        $waitUntil = (Get-Date).AddSeconds($TimeoutSeconds)
+    }
+    
+    $sleepTime = ($waitUntil - (Get-Date))
+    
+    if ($sleepTime -gt 0) {
+        Write-Host "Sleeping $([math]::Ceiling($sleepTime.TotalSeconds)) seconds..."
+        Start-Sleep -Milliseconds $sleepTime.TotalMilliseconds
+    }
 }
