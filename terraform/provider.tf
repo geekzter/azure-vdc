fda2ebd289b46667b2cc276852ad84ae14ddd345
Eric van Wijk <eric@van-wijk.com>
2020-07-11 21:47:12 +0200

    != 2.18

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,10 +4,10 @@
 # This provider block uses the following environment variables: 
 # ARM_SUBSCRIPTION_ID, ARM_CLIENT_ID, ARM_CLIENT_SECRET and ARM_TENANT_ID
 #
-provider "azurerm" {
+provider azurerm {
     # Freeze version until this issue is fixed:
     # https://github.com/terraform-providers/terraform-provider-azurerm/issues/7691
-    version = "= 2.17"
+    version = "~> 2.17, != 2.18"
     features {
         virtual_machine {
             # Don't do this in production
