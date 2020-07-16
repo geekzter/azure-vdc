ecbd54799d252a32e18da3480119bd74925829e8
Eric van Wijk <eric@van-wijk.com>
2020-07-16 16:17:03 +0200

    List subscriptions if ARM_SUBSCRIPTION_ID not set

--- scripts/functions.ps1
+++ scripts/functions.ps1
@@ -32,6 +32,7 @@ function AzLogin (
             Write-Host "Using subscription '$(az account show --query "name" -o tsv)'"
         } else {
             # Active subscription may not be the desired one
+            az account list -o tsv --query "sort_by([].{id:id, name:name},&name)" 
             Write-Warning "Using subscription '$(az account show --query "name" -o tsv)', set `$env:ARM_SUBSCRIPTION_ID if you want another one"
             Start-Sleep -Seconds 1
         }
