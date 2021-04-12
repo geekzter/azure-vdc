0d462766d73ff332dcff59e50393dde087d5e6bf
Eric van Wijk <eric@van-wijk.com>
2021-04-12 20:44:13 +0200

    azurerm 2.55 in provider.tf only

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
-    azuread                    = "= 1.3.0"
-    azurerm                    = "= 2.50.0" # "~> 2.54"
+    # azuread                    = "= 1.3.0"
+    # azurerm                    = "= 2.50.0"
+    azurerm                    = "~> 2.55"
     dns                        = "= 3.1.0"  # "~> 3.1"
     external                   = "= 2.1.0"  # "~> 2.1"
     http                       = "= 2.1.0"  # "~> 2.1"
