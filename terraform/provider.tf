2401ddb5c166c11ccc31eb09bdedeeec655ec9c6
Eric van Wijk <eric@van-wijk.com>
2021-04-12 13:49:48 +0200

    Pin versions to last known sucessful destroy

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,13 @@
 terraform {
   required_providers {
-    azurerm                    = "~> 2.54"
-    dns                        = "~> 3.1"
-    external                   = "~> 2.1"
-    http                       = "~> 2.1"
-    null                       = "~> 3.1"
-    random                     = "~> 3.1"
-    time                       = "~> 0.7"
+    azuread                    = "= 1.3.0"
+    azurerm                    = "= 2.50.0" # "~> 2.54"
+    dns                        = "= 3.1.0"  # "~> 3.1"
+    external                   = "= 2.1.0"  # "~> 2.1"
+    http                       = "= 2.1.0"  # "~> 2.1"
+    null                       = "= 3.1.0"  # "~> 3.1"
+    random                     = "= 3.1.0"  # "~> 3.1"
+    time                       = "= 0.7.0"  # "~> 0.7"
   }
   required_version             = "~> 0.14"
 }
